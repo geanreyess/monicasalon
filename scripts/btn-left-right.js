ef1d18bd8e6f03cbdf6893b2db2229a5e0e0b1a5
Gean Reyes <sagitario1990_11@hotmail.com>
2022-04-05 20:18:14 -0500

    Se culmino de arrglar la seccion de servicios

--- scripts/btn-left-right.js
+++ scripts/btn-left-right.js
@@ -5,46 +5,72 @@
     const $btnArrowRight = d.querySelector(".btn-arrow-right");
     const $carouselCards = d.querySelector(".services .carousel-cards");
 
-
+    let arr = [];
+    if (window.innerWidth < 768) {
+        arr = [1, 2, 3, 4, 5, 6]; // Numero de cards.
+    }else if ((window.innerWidth >= 768) && (window.innerWidth < 1024)) {
+        arr = [1, 2, 3]; // Numero de cards.
+    }else if (window.innerWidth >= 1024){
+        arr = [1, 2]; // Numero de cards.
+    }
+    
+    console.log(window.innerWidth)
     let count = 0;
-    const arr = [1, 2, 3, 4, 5, 6]; // Numero de cards.
 
-    $btnArrowLeft.addEventListener("click", e => {
-        const mvLeft = 100;
+    $btnArrowRight.addEventListener("click", e => {
+
+        if (window.innerWidth < 768) {
+            arr = [1, 2, 3, 4, 5, 6]; // Numero de cards.
+        }else if ((window.innerWidth >= 768) && (window.innerWidth < 1024)) {
+            arr = [1, 2, 3]; // Numero de cards.
+        }else if (window.innerWidth >= 1024){
+            arr = [1, 2]; // Numero de cards.
+        }
+
+        const mvRight = 100;
         // console.log($btnArrowLeft);
         
         
         console.log(arr.length);
         if(count < ((arr.length - 1) * 100)) {
-            count += mvLeft;
+            count += mvRight;
             let translate = `translateX(-${count}%)`;
             $carouselCards.style.setProperty("transform", translate);
             $carouselCards.style.transition = "transform .5s ease-in-out";
             
-            $btnArrowRight.classList.remove("opacity");
+            $btnArrowLeft.classList.remove("opacity");
             if(count === ((arr.length - 1) * 100)){
-                $btnArrowLeft.classList.add("opacity");
+                $btnArrowRight.classList.add("opacity");
             }
             // console.log($carouselCards.style);
         }
 
     });
     
-    $btnArrowRight.addEventListener("click", e => {
-        const mvRight = 100;
+    $btnArrowLeft.addEventListener("click", e => {
+
+        if (window.innerWidth < 768) {
+            arr = [1, 2, 3, 4, 5, 6]; // Numero de cards.
+        }else if ((window.innerWidth >= 768) && (window.innerWidth < 1024)) {
+            arr = [1, 2, 3]; // Numero de cards.
+        }else if (window.innerWidth >= 1024){
+            arr = [1, 2]; // Numero de cards.
+        }
+        
+        const mvLeft = 100;
         // console.log($btnArrowRight);
 
         console.log(count);
 
         if(count >0 ){
-            count -= mvRight;
+            count -= mvLeft;
             let translate = `translateX(-${count}%)`;
             $carouselCards.style.setProperty("transform", translate);
             $carouselCards.style.transition = "transform .5s ease-in-out";
 
-            $btnArrowLeft.classList.remove("opacity");
+            $btnArrowRight.classList.remove("opacity");
             if(count === 0){
-                $btnArrowRight.classList.add("opacity");
+                $btnArrowLeft.classList.add("opacity");
             }
         }
         
